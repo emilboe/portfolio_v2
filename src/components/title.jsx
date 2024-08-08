e9f04ecd716addc80183d6db5fb05142030b51ad
Emil Boen <emil@boen.net>
2024-08-08 18:30:21 +0200

    updated copy and solved babel conflict

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -8,13 +8,12 @@ const Title = memo(() => {
       <div className="title_background"></div>
       <div className="title_filter"></div>{" "}
       <div className="title_contents">
-        <h1>Ambitious & Curious Developer</h1>
-        <div className="line"></div>
         <h1>Emil Bøen</h1>
-        <h4>
-          Hey! I'm a freshly graduated front-end developer based in Norway.
-          <br /> I always enjoy trying new things and I love coming up with new ideas.
-        </h4>
+        <div className="line"></div>
+        <h2>
+          Hi! I'm a front-end developer based in Norway with 2 years of professional experience.
+          <br />I’m passionate about exploring new technologies and constantly innovating.
+        </h2>
       </div>
     </section>
   );
